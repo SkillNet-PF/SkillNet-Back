9f3e80e9e433871aa326ac46f3e72c031cec567d
Sasha Davila <sasha@example.com>
2025-10-02 13:41:02 -0300

    resource serviceprovider

---
+++ src/serviceprovider/serviceprovider/serviceprovider.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ServiceproviderService } from './serviceprovider.service';
+import { CreateServiceproviderDto } from './dto/create-serviceprovider.dto';
+import { UpdateServiceproviderDto } from './dto/update-serviceprovider.dto';
+
+@Controller('serviceprovider')
+export class ServiceproviderController {
+  constructor(private readonly serviceproviderService: ServiceproviderService) {}
+
+  @Post()
+  create(@Body() createServiceproviderDto: CreateServiceproviderDto) {
+    return this.serviceproviderService.create(createServiceproviderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.serviceproviderService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.serviceproviderService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateServiceproviderDto: UpdateServiceproviderDto) {
+    return this.serviceproviderService.update(+id, updateServiceproviderDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.serviceproviderService.remove(+id);
+  }
+}
