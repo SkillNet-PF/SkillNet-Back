438e8e91951ad3a5ee3fc5011f8402c4727db7d0
Sasha Davila <sasha@example.com>
2025-10-02 19:13:16 -0300

    config de db forma local y providerservice

--- src/serviceprovider/serviceprovider/serviceprovider.controller.ts
+++ src/serviceprovider/serviceprovider/serviceprovider.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { ServiceproviderService } from './serviceprovider.service';
 import { CreateServiceproviderDto } from './dto/create-serviceprovider.dto';
 import { UpdateServiceproviderDto } from './dto/update-serviceprovider.dto';
@@ -12,22 +12,22 @@ export class ServiceproviderController {
     return this.serviceproviderService.create(createServiceproviderDto);
   }
 
-  @Get()
+  @Get()//trae todos los porvedores 
   findAll() {
     return this.serviceproviderService.findAll();
   }
 
-  @Get(':id')
+  @Get(':id')//busca provedor por categoria 
   findOne(@Param('id') id: string) {
     return this.serviceproviderService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')//actualizar datos del perfil 
   update(@Param('id') id: string, @Body() updateServiceproviderDto: UpdateServiceproviderDto) {
     return this.serviceproviderService.update(+id, updateServiceproviderDto);
   }
 
-  @Delete(':id')
+  @Delete(':id')// solo admin 
   remove(@Param('id') id: string) {
     return this.serviceproviderService.remove(+id);
   }
