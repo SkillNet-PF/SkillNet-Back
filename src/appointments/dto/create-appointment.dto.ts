215c509d93e362dd7cfcd3a06f3ba68cd5dad4aa
Sofia Ruiz <sofiaruiz.1505@gmail.com>
2025-10-04 17:35:12 -0300

    creacion del CreateAppointmentDto

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,6 @@
-export class CreateAppointmentDto {}
+export class CreateAppointmentDto {
+    category: string; //nombre de la categoria
+    appointmentDate:Date;
+    notes: string;
+    provider: string; //nombre del proveedor
+}
