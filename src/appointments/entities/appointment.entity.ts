5065a4ed027f3bc2e09e97ee1324b16bf035e65d
Sofia Ruiz <sofiaruiz.1505@gmail.com>
2025-10-04 17:06:00 -0300

    Creacion del enum status, creacion de la entidad appointment

---
+++ src/appointments/entities/appointment.entity.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {v4 as uuid} from 'uuid'
+import { Status } from "./status.enum";
+@Entity({
+    name: 'appointments'
+})
+
+export class Appointment {
+
+    @PrimaryGeneratedColumn('uuid')
+    AppointmentID: string = uuid();
+
+    @ManyToOne(()=> Categories, (category) => category.appointments)
+    @JoinColumn({name: 'category_id'})
+    CategoryID: Category;
+
+    @Column({
+        type:'varchar',
+        nullable: false
+    })
+    CreationDate: string;
+
+    @Column({
+        type: 'date',
+        nullable: false
+    })
+    AppointmentDate: Date;
+
+    @Column({
+        type: 'varchar',
+        nullable:false
+    })
+    Notes: string;
+
+    @Column({
+        type: 'enum',
+        enum: Status,
+        default: Status.PENDING
+    })
+    Status: Status;
+
+    @ManyToOne(()=> Client, (client)=>client.appointments)
+    ClientID: Client;
+
+    @ManyToOne(()=> Provider, (provider)=> provider.)
+    ProviderID:Provider ;
+}
