23af0e5d83a0e58031dd91cad36ea7eec966c5cd
Sasha Davila <sasha@example.com>
2025-10-06 21:15:53 -0300

    Actualización del módulo serviceprovider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import typeormConfig from './config/typeorm';
 import { ServiceproviderModule } from './serviceprovider/serviceprovider/serviceprovider.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeorm from './config/typeorm'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get('typeorm') || {},
+    }),
+    AuthModule,
+  ],
   imports: [ServiceproviderModule,
     ConfigModule.forRoot({isGlobal: true, load: [typeorm]}),
     TypeOrmModule.forRootAsync({
