934536fb048f6356db8bd3eb5ded3e2eeaaa0911
Sofia Ruiz <sofiaruiz.1505@gmail.com>
2025-10-13 22:31:03 -0300

    commit luego del merge - creacion del seeder de categorias y subscripciones

--- src/seeds/seeds.module.ts
+++ src/seeds/seeds.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { InitialDataSeed } from './seeds.service';
-// import { SeedsController } from './seeds.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categories } from 'src/categories/entities/categories.entity';
 import { subscriptions } from 'src/subscription/entities/subscription.entity';
