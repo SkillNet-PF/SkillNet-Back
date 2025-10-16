cbfba151d030707f802f57d14578bd76608913d5
Sofia Ruiz <sofiaruiz.1505@gmail.com>
2025-10-16 17:40:28 -0300

    Logica de la creacion de subscripciones

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { subscriptions } from './entities/subscription.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SubscriptionService {
-  create(createSubscriptionDto: CreateSubscriptionDto) {
-    return 'This action adds a new subscription';
+  constructor(
+    @InjectRepository(subscriptions)
+    private readonly subscriptionsRepository: Repository<subscriptions>,
+  ){}
+  async create(createSubscriptionDto: CreateSubscriptionDto) {
+    const {Name, Descption, monthlyServices, price} = createSubscriptionDto;
+
+    if (!Name || !Descption || !monthlyServices || !price) {
+      throw new Error('All fields are required');
+    }
+    
+    if (!Number(monthlyServices) || !Number(price)) {
+      throw new Error('monthlyServices and price must be numbers');
+    }
+
+    const subscription = new subscriptions();
+    subscription.Name = Name;
+    subscription.Descption = Descption;
+    subscription.monthlyServices = Number(monthlyServices);
+    subscription.price = Number(price);
+    subscription.isActive = true;
+    
+    await this.subscriptionsRepository.save(subscription);
+    return subscription;
   }
 
   findAll() {
