def8cf4d2b057dd75e33b30ee47669be45d811cd
Sofia Ruiz <sofiaruiz.1505@gmail.com>
2025-10-20 17:19:04 -0300

    contadores para el dashboard

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Appointment } from 'src/appointments/entities/appointment.entity';
+import { User } from 'src/auth/entities/user.entity';
+import { Client } from 'src/clients/entities/client.entity';
+import { UserRole } from 'src/common/enums/user-role.enum';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AdminService {
-  getDashboard() {
-    throw new Error('Method not implemented.');
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Appointment)
+    private readonly appointmentRepository: Repository<Appointment>,
+
+  ){}
+  async getDashboard() {
+    
+    // Totales
+    const totalClients = await this.userRepository.count({ where: { rol: UserRole.client } });
+    const totalProviders = await this.userRepository.count({ where: { rol: UserRole.provider} });
+    
+    const users = await this.userRepository.find();
+    
+    let ingresos = 0
+    
+    users.forEach(user => {
+      if (user.rol === 'client') {
+        let client = user as Client
+        
+        if (client.suscription) {
+          ingresos = ingresos + client.suscription?.price
+        }
+      }
+      
+      
+    });
+    const totalAppointments = await this.appointmentRepository.count();
+    
+    return ({
+      totalClients,
+      totalProviders,
+      ingresos,
+      totalAppointments
+    })
   }
 }
